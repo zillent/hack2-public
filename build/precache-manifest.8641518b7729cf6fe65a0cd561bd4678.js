5991ca2447d798e74a8febe5a3617ac669958493
Sergey Kozlov <serg@zillent.ru>
2020-06-20 05:09:44 +0500

    fixes

---
+++ build/precache-manifest.8641518b7729cf6fe65a0cd561bd4678.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea80f6517afd55a5b991a8da6ea5be0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a63e4279e1e73dbfd8a",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e94995d322931e0c9304",
+    "url": "/static/css/main.74c02955.chunk.css"
+  },
+  {
+    "revision": "9a63e4279e1e73dbfd8a",
+    "url": "/static/js/2.5152787d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5152787d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94995d322931e0c9304",
+    "url": "/static/js/main.5ebc8199.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  },
+  {
+    "revision": "956dc05e93c67505cf9f956111a6dadd",
+    "url": "/static/media/bell.956dc05e.svg"
+  },
+  {
+    "revision": "ca672c88ef23debba3fe88ce3938643c",
+    "url": "/static/media/logo.ca672c88.svg"
+  },
+  {
+    "revision": "ddc4da12894fe5abf74259da720fd006",
+    "url": "/static/media/user.ddc4da12.svg"
+  }
+]);
