0874f26de3ea2623ad14f3c90a283ffd28663695
Sergey Kozlov <serg@zillent.ru>
2020-06-20 12:12:55 +0500

    fixes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.08f4cbc3f5147b093025abb361ccaae9.js"
+  "/precache-manifest.8bddc1ada3efbdca70a72c7de0187a49.js"
 );
 
 self.addEventListener('message', (event) => {
